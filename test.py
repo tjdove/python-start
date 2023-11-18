d8df5c27c6237e7027e129da0472f9e0ce8edf02
Tim Dove <timdoveemail@yahoo.com>
2023-11-17 22:33:24 -0500

    FIrst Commit

---
+++ test.py
@@ -0,0 +1,6 @@
+print("Hello World")
+
+data = {"hey":12313, "he2y":12313,"hey":12313, "he2y":12313,"hey":12313, "he2y":12313,"hey":12313, "he2y":12313,"hey":12313, "he2y":12313}
+
+print(data).print
+
